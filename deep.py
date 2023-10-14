8742830fe28ae1d6fa43015bdde1a022cbc88b00
test <test>
2023-10-14 12:18:59 +0700

    test

---
+++ deep.py
@@ -0,0 +1,7 @@
+infinity = input("Какой ответ на главный вопрос жизни, вселенной и всего такого?")
+if infinity == "42":
+    print(" Да")
+elif infinity == "сорок два":
+    print(" Да")
+else:
+    print(" Нет")
